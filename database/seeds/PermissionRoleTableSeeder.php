cc037b6e2117cc8958186702eca2565d560cff94
SuhelMehta9 <suhelmehta@outlook.com>
2019-10-27 17:51:14 +0530

    Changed default permission of user

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -11,7 +11,7 @@ class PermissionRoleTableSeeder extends Seeder
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
         $user_permissions = $admin_permissions->filter(function ($permission) {
-            return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_';
+            return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->title, 0, 11) != 'permission_' && substr($permission->title,0,9) != 'location_' && substr($permission->title, 0, 8)!= 'working_' && substr($permission->title, 8, 11) != 'edit' && substr($permission->title, 8, 13) != 'delete';
         });
         Role::findOrFail(2)->permissions()->sync($user_permissions);
     }
